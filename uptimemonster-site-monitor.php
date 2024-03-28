d660b460cbf99c29d9b6b4e967aa5db64932b1ea
Faisal Hossain Shuvo <shuvo586@gmail.com>
2024-03-28 11:17:21 +0600

    fixed missing license information

--- uptimemonster-site-monitor.php
+++ uptimemonster-site-monitor.php
@@ -5,6 +5,8 @@
  * Description: WordPress Site Activity Monitor For UptimeMonster
  * Author: UptimeMonster
  * Author URI: https://uptimemonster.com/
+ * License: GPLv2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: uptimemonster-site-monitor
  * Domain Path: /languages
  * Version: 1.0.0
@@ -14,6 +16,22 @@
  * Tested Upto: 6.4
  *
  * @package UptimeMonster\SiteMonitor
+ *
+ * Copyright (C) 2024 UptimeMonster
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ *
  */
 
 use UptimeMonster\SiteMonitor\UptimeMonster_Site_Monitor;
